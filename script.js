97772316ed32c79778f97be5896e1107095f6336
Arinze <calistusarinzechinweuba@gmail.com>
2023-06-10 07:10:40 +0100

    Add Fizz condition

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const answer = parseInt(prompt("Please enter a number you would like to FizzBuzz up to:"));
 
 for(let i = 1; i <= answer; i++){
-    if(i % 3 === 0){
+    if(i % 3 === 0 && i % 5 === 0 ){
+       console.log("FizzBuzz");
+    }else if(i % 3 === 0){
        console.log("Fizz");
     }
 
