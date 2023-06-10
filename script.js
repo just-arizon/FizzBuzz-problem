ffba39dd66cf41708bb18539cfccb6522b42224d
Arinze <calistusarinzechinweuba@gmail.com>
2023-06-10 07:11:36 +0100

    add Buzz condition

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ for(let i = 1; i <= answer; i++){
        console.log("FizzBuzz");
     }else if(i % 3 === 0){
        console.log("Fizz");
+    }else if(i % 5 === 0){
+       console.log("Buzz");
     }
 
 }
